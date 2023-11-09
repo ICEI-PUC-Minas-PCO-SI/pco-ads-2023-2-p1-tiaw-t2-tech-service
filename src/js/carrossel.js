11d4b4818300a17c2743d674be0f7938dcc49394
alice <lilimachado83@gmail.com>
2023-11-08 23:54:34 -0300

    alt

--- src/js/carrossel.js
+++ src/js/carrossel.js
@@ -1,10 +1,7 @@
 let bancoVagas = JSON.parse(localStorage.getItem("usuario")) || [];
-console.log(bancoVagas);
 let slideAtual = 0;
-let loadingOverlay = document.getElementById("loading-overlay");
 
 function criarSlide(slideAtual) {
-  console.log(bancoVagas.imagem);
   var strCarrossel = "";
   let slide = document.createElement("div");
   slide.className = "carousel-item" + (slideAtual === 0 ? " active" : "");
@@ -13,37 +10,58 @@ function criarSlide(slideAtual) {
   let divConteudo = document.getElementById("tela");
 
   if (bancoVagas && bancoVagas.Vaga) {
-    console.log(bancoVagas.Vaga);
-    for (let i = 0; i < bancoVagas.Vaga.length; i++) {
-      const vagaAtual = bancoVagas.Vaga[i];
+       i = 0;
+      const vagaAtual = bancoVagas.Vaga[slideAtual];
       console.log(vagaAtual);
       strCarrossel += `
-      <div class="vaga-disponivel" data-identificador="${i}">
-        <div class="div-img">
-          <img src="${vagaIMAGEM}" alt="Img-Usuario" class="img-vaga"/>
+      <div class="vaga-disponivel container bg-light p-4 w-75 h-75 mt-5 rounded-5"" data-identificador="${i}">
+        <div class="header d-flex justify-content-around my-5">
+                 <i class="fas fa-fire fs-2"></i>
+                 <i class="fas fa-comments fs-2"></i>
+                <i class="fas fa-user fs-2"></i>
         </div>
-        <div class="div-dadosVagas">
-          <p class="paragraph-titulo"><strong>Nome da Vaga : </strong>${
-            vagaAtual.nomeVaga
-          }</p>
-          <p class="paragraph-descricao"><strong>Descrição : </strong>${
-            vagaAtual.descricao
-          }</p>
-          <p class="paragraph-filtro"><strong>Filtro : </strong>${vagaAtual.filtro.join(
-            ", "
-          )}</p>
-          <p class="paragraph-software"><strong>Linguagens : </strong> ${vagaAtual.software.join(
-            ", "
-          )}</p>
-          <p class="paragraph-site"><strong>Site : </strong>${
-            vagaAtual.site
-          }</p>
-          <button class="button-vaga" onclick="removerVaga(${i})">Remover</button>
-        </div>
+        <div class="row">
+                <div class="col-md-6 mx-0 my-auto">
+                    <div class="d-flex justify-content-center">
+                        <img src="${vagaIMAGEM}" width="250px" height="250px" alt="imagem-vaga" class="img-fluid rounded-5"/>
+                    </div>
+                </div>
+                <div class="div-dadosVagas col-md-6">
+                <h3>Descrição da Vaga</h3>
+            <p class="paragraph-titulo"><strong>Nome da Vaga : </strong>${
+                vagaAtual.nomeVaga
+            }</p>
+            <p class="paragraph-descricao"><strong>Descrição : </strong>${
+                vagaAtual.descricao
+            }</p>
+            <p class="paragraph-filtro"><strong>Filtro : </strong>${vagaAtual.filtro.join(
+                ", "
+            )}</p>
+            <p class="paragraph-software"><strong>Linguagens : </strong> ${vagaAtual.software.join(
+                ", "
+            )}</p>
+            <p class="paragraph-site"><strong>Site : </strong>${
+                vagaAtual.site
+            }</p>
+            </div>
+            <div class="actions d-flex justify-content-around">
+                  <div class="action">
+                       <i class="fas fa-times text-danger fs-2" onclick="recusarVaga()"></i>
+                  </div>
+                  <div class="action">
+                      <i class="fas fa-heart text-success fs-2" onclick="aceitarVaga()"></i>
+                  </div>
+              </div>
       </div>`;
-    }
-  } else {
-    console.log("Nenhum dado de vaga encontrado.");
+  } else{
+    strCarrossel = `<div class="container bg-light p-4 w-75 h-75 mt-5 rounded-5">
+    <div class="header d-flex justify-content-around my-5">
+                  <i class="fas fa-fire fs-2"></i>
+                  <i class="fas fa-comments fs-2"></i>
+                  <i class="fas fa-user fs-2"></i>
+              </div>
+              <h2 class="text-center">Você chegou ao fim, essas foram as vagas disponíveis. Obrigado por colaborar com o nosso site!</h2>
+          </div>`;
   }
   divConteudo.innerHTML = strCarrossel;
   console.log(divConteudo);
