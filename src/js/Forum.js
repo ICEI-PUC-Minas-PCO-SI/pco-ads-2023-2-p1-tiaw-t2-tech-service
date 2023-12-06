3ad973a59e6c06874526a7fc5521a30e1c6120b1
GustavoOCastro <103152662+GustavoOCastro@users.noreply.github.com>
2023-12-05 21:43:54 -0300

    Update Forum.js

--- src/js/Forum.js
+++ src/js/Forum.js
@@ -16,6 +16,120 @@ function closeForm() {
   document.getElementById("createPostForm").style.display = "none";
 }
 
-function filterAll() {}
+function filterAll() {
+  fetch('http://localhost:3000/posts')
+  .then(response => response.json())
+      .then(posts => {
+              updateUI(posts);
+            });
+  }
 
-function filterRecent() {}
+function updateUI(posts) {
+
+  var forumContainer = document.querySelector('.container');
+    forumContainer.innerHTML = '';
+
+  posts.forEach(post => {
+        var forumDiv = document.createElement('div');
+        forumDiv.className = 'forum';
+
+                    var titleHeading = document.createElement('h2');
+        titleHeading.textContent = post.title;
+
+                    var contentParagraph = document.createElement('p');
+        contentParagraph.textContent = post.content;
+
+                    var infoDiv = document.createElement('div');
+        infoDiv.className = 'info';
+
+                    var commentsSpan = document.createElement('span');
+        commentsSpan.textContent = 'Comentários: ' + post.commentsCount; 
+
+                    var likesSpan = document.createElement('span');
+        likesSpan.textContent = 'Curtidas: ' + post.likesCount; 
+
+                    var viewsSpan = document.createElement('span');
+        viewsSpan.textContent = 'Visualizações: ' + post.viewsCount;
+
+                    infoDiv.appendChild(commentsSpan);
+        infoDiv.appendChild(likesSpan);
+        infoDiv.appendChild(viewsSpan);
+
+                    forumDiv.appendChild(titleHeading);
+        forumDiv.appendChild(contentParagraph);
+        forumDiv.appendChild(infoDiv);
+
+                    forumContainer.appendChild(forumDiv);
+      });
+  }
+
+function filterRecent() {
+
+  fetch('http://localhost:3000/posts?_sort=date&_order=desc')
+      .then(response => response.json())
+      .then(recentPosts => {
+
+                  updateUI(recentPosts);
+            });
+  }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+                            filterAll(); 
+                            setupEventListeners(); 
+                          });
+
+function renderPosts(posts) {
+
+  var forumContainer = document.querySelector('.container');
+    forumContainer.innerHTML = '';
+
+  posts.forEach(post => {
+        var forumDiv = document.createElement('div');
+        forumDiv.className = 'forum';
+
+                    var titleHeading = document.createElement('h2');
+        titleHeading.textContent = post.title;
+
+                    var contentParagraph = document.createElement('p');
+        contentParagraph.textContent = post.content;
+
+                    var infoDiv = document.createElement('div');
+        infoDiv.className = 'info';
+
+                    var commentsSpan = document.createElement('span');
+        commentsSpan.textContent = 'Comentários: ' + post.commentsCount;
+
+                    var likesSpan = document.createElement('span');
+        likesSpan.textContent = 'Curtidas: ' + post.likesCount;
+
+                    var viewsSpan = document.createElement('span');
+        viewsSpan.textContent = 'Visualizações: ' + post.viewsCount;
+
+                    infoDiv.appendChild(commentsSpan);
+        infoDiv.appendChild(likesSpan);
+        infoDiv.appendChild(viewsSpan);
+
+                    forumDiv.appendChild(titleHeading);
+        forumDiv.appendChild(contentParagraph);
+        forumDiv.appendChild(infoDiv);
+
+                    forumContainer.appendChild(forumDiv);
+      });
+  }
+
+function fetchAndRenderPosts(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(posts => {
+              renderPosts(posts);
+            })
+      .catch(error => {
+              console.error('Erro ao buscar posts:', error);
+            });
+  }
+
+function setupEventListeners() {
+
+}
