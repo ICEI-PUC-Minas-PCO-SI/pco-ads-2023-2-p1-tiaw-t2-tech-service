7a214ff1497f792c6d103d64957deaf338781655
alice <lilimachado83@gmail.com>
2023-12-13 03:27:31 -0300

    FEAT: carrossel integrado ao json-server

--- src/js/carrossel.js
+++ src/js/carrossel.js
@@ -3,74 +3,75 @@ let params = new URLSearchParams(location.search);
 console.log(params);
 let id = params.get("id");
 console.log(id);
-let linkVagasDis = document.getElementById("a-Vagas");
-linkVagasDis.href = `VagasDisponiveis.html?id=${id}`;
-let linkPerfil = document.getElementById("a-Perfil");
-linkPerfil.href = `Perfil.Freelancer.html?id=${id}`;
-console.log(linkPerfil);
+// let linkVagasDis = document.getElementById("a-Vagas");
+// // linkVagasDis.href = `VagasDisponiveis.html?id=${id}`;
+// let linkPerfil = document.getElementById("a-Perfil");
+// linkPerfil.href = `Perfil.Freelancer.html?id=${id}`;
+// console.log(linkPerfil);
 
-//Recuperando ID do usuario
-let bancoVagas = JSON.parse(localStorage.getItem("usuario")) || [];
 let slideAtual = 0;
+let URLVagas = 'http://localhost:3000/vagas';
+
+criarSlide(slideAtual);
 
 function criarSlide(slideAtual) {
   var strCarrossel = "";
   let slide = document.createElement("div");
   slide.className = "carousel-item" + (slideAtual === 0 ? " active" : "");
-  const vaga = bancoVagas[slideAtual];
-  const vagaIMAGEM = bancoVagas.imagem;
-  let divConteudo = document.getElementById("tela");
   slide.classList.add("fade");
 
-  if (bancoVagas && bancoVagas.Vaga) {
-    i = 0;
-    const vagaAtual = bancoVagas.Vaga[slideAtual];
-    console.log(vagaAtual);
-    strCarrossel += `
-      <div class="vaga-disponivel container bg-light p-4 w-75 h-75 mt-5 rounded-5"" data-identificador="${i}">
-        <div class="header d-flex justify-content-around my-5">
-                 <i class="fas fa-fire fs-2"></i>
-                 <i class="fas fa-comments fs-2"></i>
-                 <a href="PerfilDaEmpresa.html"><i class="fas fa-user fs-2"></i></a>
-        </div>
-        <div class="row">
-                <div class="col-md-6 mx-0 my-auto">
-                    <div class="d-flex justify-content-center">
-                        <img src="${vagaIMAGEM}" width="250px" height="250px" alt="imagem-vaga" class="img-fluid rounded-5 mb-3"/>
+  let divConteudo = document.getElementById("tela");
+
+  fetch(URLVagas)
+  .then(res => res.json())
+  .then(function(dados){
+    vagas = dados;
+
+    if (id != null) {
+        if(vagas[slideAtual].status){
+          strCarrossel = `
+        <div class="vaga-disponivel container bg-light p-4 w-75 h-75 mt-5 rounded-5"" data-identificador="${slideAtual}">
+          <div class="header d-flex justify-content-around my-5">
+                   <i class="fas fa-fire fs-2"></i>
+                   <i class="fas fa-comments fs-2"></i>
+                   <a href="PerfilDaEmpresa.html"><i class="fas fa-user fs-2"></i></a>
+          </div>
+          <div class="row">
+                  <div class="col-md-6 mx-0 my-auto">
+                      <div class="d-flex justify-content-center">
+                          <img src="${vagas[slideAtual].imagem}" width="250px" height="250px" alt="imagem-vaga" class="img-fluid rounded-5 mb-3"/>
+                      </div>
+                  </div>
+                  <div class="div-dadosVagas col-md-6">
+                  <h3 class="my-3">Detalhes</h3>
+              <p class="paragraph-titulo"><strong>Nome da Vaga : </strong>${
+                vagas[slideAtual].nomeVaga
+              }</p>
+              <p class="paragraph-descricao"><strong>Descrição : </strong>${
+                vagas[slideAtual].descricao
+              }</p>
+              <p class="paragraph-filtro"><strong>Categoria : </strong>${vagas[slideAtual].filtro}</p>
+              <p class="paragraph-site"><strong>Site da Empresa : </strong>
+                <a href="${vagas[slideAtual].site}" target="_blank">${vagas[slideAtual].site}</a>
+              </p>
+              </div>
+              <div class="actions d-flex justify-content-around">
+                    <div class="action">
+                         <i class="fas fa-times text-danger fs-2" onclick="recusarVaga()"></i>
+                    </div>
+                    <div class="action">
+                        <i class="fas fa-heart text-success fs-2" onclick="aceitarVaga()"></i>
                     </div>
                 </div>
-                <div class="div-dadosVagas col-md-6">
-                <h3 class="my-3">Detalhes</h3>
-            <p class="paragraph-titulo"><strong>Nome da Vaga : </strong>${
-              vagaAtual.nomeVaga
-            }</p>
-            <p class="paragraph-descricao"><strong>Descrição : </strong>${
-              vagaAtual.descricao
-            }</p>
-            <p class="paragraph-filtro"><strong>Filtro : </strong>${vagaAtual.filtro.join(
-              ", "
-            )}</p>
-            <p class="paragraph-software"><strong>Linguagens : </strong> ${vagaAtual.software.join(
-              ", "
-            )}</p>
-            <p class="paragraph-site"><strong>Site : </strong>${
-              vagaAtual.site
-            }</p>
-            </div>
-            <div class="actions d-flex justify-content-around">
-                  <div class="action">
-                       <i class="fas fa-times text-danger fs-2" onclick="recusarVaga()"></i>
-                  </div>
-                  <div class="action">
-                      <i class="fas fa-heart text-success fs-2" onclick="aceitarVaga()"></i>
-                  </div>
-              </div>
-      </div>`;
-  }
-  divConteudo.innerHTML = strCarrossel;
-  setTimeout(() => {
-    slide.classList.remove("fade");
-  }, 100);
+        </div>`;
+        }
+    }else{
+      strCarrossel = `<p>Você precisa estar cadastrado para ter acesso a esse conteúdo.</p>`;
+    }
+    divConteudo.innerHTML = strCarrossel;
+  })
+
+  
 }
 
 function abrirModal(){
@@ -94,7 +95,7 @@ function aceitarVaga() {
   slideAtual++;
   let divConteudo = document.getElementById("tela");
 
-  if (bancoVagas.nomeVaga == undefined) {
+  if (slideAtual >= vagas.length) {
     strCarrossel = `<div class="container bg-light p-4 w-75 h-75 mt-5 rounded-5">
             <div class="header d-flex justify-content-around my-5">
                 <i class="fas fa-fire fs-2"></i>
@@ -115,7 +116,7 @@ function recusarVaga() {
   abrirModalRecusa();
   slideAtual++;
 
-  if (bancoVagas.nomeVaga == undefined) {
+  if (slideAtual >= vagas.length) {
     strCarrossel = `<div class="container bg-light p-4 w-75 h-75 mt-5 rounded-5">
             <div class="header d-flex justify-content-around my-5">
                 <i class="fas fa-fire fs-2"></i>
@@ -133,4 +134,7 @@ function recusarVaga() {
 document.addEventListener("DOMContentLoaded", function () {
   criarSlide(slideAtual);
 });
+
+
+
 
