0572abb895f0060067dd5ed5759652b45c4c9d03
RafaeL Brandao <rbs13bg13@gmail.com>
2023-12-13 22:54:09 -0300

    Modificaçoes

--- src/js/Forum.js
+++ src/js/Forum.js
@@ -28,104 +28,104 @@ function filterAll() {
     });
 }
 
-function updateUI(posts) {
-  var forumContainer = document.querySelector(".container");
-  forumContainer.innerHTML = "";
+// function updateUI(posts) {
+//   var forumContainer = document.querySelector(".container");
+//   forumContainer.innerHTML = "";
 
-  posts.forEach((post) => {
-    var forumDiv = document.createElement("div");
-    forumDiv.className = "forum";
+//   posts.forEach((post) => {
+//     var forumDiv = document.createElement("div");
+//     forumDiv.className = "forum";
 
-    var titleHeading = document.createElement("h2");
-    titleHeading.textContent = post.title;
+//     var titleHeading = document.createElement("h2");
+//     titleHeading.textContent = post.title;
 
-    var contentParagraph = document.createElement("p");
-    contentParagraph.textContent = post.content;
+//     var contentParagraph = document.createElement("p");
+//     contentParagraph.textContent = post.content;
 
-    var infoDiv = document.createElement("div");
-    infoDiv.className = "info";
+//     var infoDiv = document.createElement("div");
+//     infoDiv.className = "info";
 
-    var commentsSpan = document.createElement("span");
-    commentsSpan.textContent = "Comentários: " + post.commentsCount;
+//     var commentsSpan = document.createElement("span");
+//     commentsSpan.textContent = "Comentários: " + post.commentsCount;
 
-    var likesSpan = document.createElement("span");
-    likesSpan.textContent = "Curtidas: " + post.likesCount;
+//     var likesSpan = document.createElement("span");
+//     likesSpan.textContent = "Curtidas: " + post.likesCount;
 
-    var viewsSpan = document.createElement("span");
-    viewsSpan.textContent = "Visualizações: " + post.viewsCount;
+//     var viewsSpan = document.createElement("span");
+//     viewsSpan.textContent = "Visualizações: " + post.viewsCount;
 
-    infoDiv.appendChild(commentsSpan);
-    infoDiv.appendChild(likesSpan);
-    infoDiv.appendChild(viewsSpan);
+//     infoDiv.appendChild(commentsSpan);
+//     infoDiv.appendChild(likesSpan);
+//     infoDiv.appendChild(viewsSpan);
 
-    forumDiv.appendChild(titleHeading);
-    forumDiv.appendChild(contentParagraph);
-    forumDiv.appendChild(infoDiv);
+//     forumDiv.appendChild(titleHeading);
+//     forumDiv.appendChild(contentParagraph);
+//     forumDiv.appendChild(infoDiv);
 
-    forumContainer.appendChild(forumDiv);
-  });
-}
+//     forumContainer.appendChild(forumDiv);
+//   });
+// }
 
-function filterRecent() {
-  fetch("http://localhost:3000/posts?_sort=date&_order=desc")
-    .then((response) => response.json())
-    .then((recentPosts) => {
-      updateUI(recentPosts);
-    });
-}
+// function filterRecent() {
+//   fetch("http://localhost:3000/posts?_sort=date&_order=desc")
+//     .then((response) => response.json())
+//     .then((recentPosts) => {
+//       updateUI(recentPosts);
+//     });
+// }
 
-document.addEventListener("DOMContentLoaded", function () {
-  filterAll();
-  setupEventListeners();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   filterAll();
+//   setupEventListeners();
+// });
 
-function renderPosts(posts) {
-  var forumContainer = document.querySelector(".container");
-  forumContainer.innerHTML = "";
+// function renderPosts(posts) {
+//   var forumContainer = document.querySelector(".container");
+//   forumContainer.innerHTML = "";
 
-  posts.forEach((post) => {
-    var forumDiv = document.createElement("div");
-    forumDiv.className = "forum";
+//   posts.forEach((post) => {
+//     var forumDiv = document.createElement("div");
+//     forumDiv.className = "forum";
 
-    var titleHeading = document.createElement("h2");
-    titleHeading.textContent = post.title;
+//     var titleHeading = document.createElement("h2");
+//     titleHeading.textContent = post.title;
 
-    var contentParagraph = document.createElement("p");
-    contentParagraph.textContent = post.content;
+//     var contentParagraph = document.createElement("p");
+//     contentParagraph.textContent = post.content;
 
-    var infoDiv = document.createElement("div");
-    infoDiv.className = "info";
+//     var infoDiv = document.createElement("div");
+//     infoDiv.className = "info";
 
-    var commentsSpan = document.createElement("span");
-    commentsSpan.textContent = "Comentários: " + post.commentsCount;
+//     var commentsSpan = document.createElement("span");
+//     commentsSpan.textContent = "Comentários: " + post.commentsCount;
 
-    var likesSpan = document.createElement("span");
-    likesSpan.textContent = "Curtidas: " + post.likesCount;
+//     var likesSpan = document.createElement("span");
+//     likesSpan.textContent = "Curtidas: " + post.likesCount;
 
-    var viewsSpan = document.createElement("span");
-    viewsSpan.textContent = "Visualizações: " + post.viewsCount;
+//     var viewsSpan = document.createElement("span");
+//     viewsSpan.textContent = "Visualizações: " + post.viewsCount;
 
-    infoDiv.appendChild(commentsSpan);
-    infoDiv.appendChild(likesSpan);
-    infoDiv.appendChild(viewsSpan);
+//     infoDiv.appendChild(commentsSpan);
+//     infoDiv.appendChild(likesSpan);
+//     infoDiv.appendChild(viewsSpan);
 
-    forumDiv.appendChild(titleHeading);
-    forumDiv.appendChild(contentParagraph);
-    forumDiv.appendChild(infoDiv);
+//     forumDiv.appendChild(titleHeading);
+//     forumDiv.appendChild(contentParagraph);
+//     forumDiv.appendChild(infoDiv);
 
-    forumContainer.appendChild(forumDiv);
-  });
-}
+//     forumContainer.appendChild(forumDiv);
+//   });
+// }
 
-function fetchAndRenderPosts(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((posts) => {
-      renderPosts(posts);
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar posts:", error);
-    });
-}
+// function fetchAndRenderPosts(url) {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((posts) => {
+//       renderPosts(posts);
+//     })
+//     .catch((error) => {
+//       console.error("Erro ao buscar posts:", error);
+//     });
+// }
 
-function setupEventListeners() {}
+// function setupEventListeners() {}
--- src/js/PerfilDaEmpresa.js
+++ src/js/PerfilDaEmpresa.js
@@ -14,37 +14,32 @@ document.addEventListener("DOMContentLoaded", function () {
   linkEditar.href = `TelaEditarPerfil.html?id=${id}`;
   let linkCriarVa = document.getElementById("a-CriarVagas");
   linkCriarVa.href = `TelaCriarVagas.html?id=${id}`;
-  console.log(linkCriarVa);
   ReceberID(id);
-  Vagas(id);
+  Servicos(id);
 });
 
 function ReceberID(id) {
-  let Json = 'http://localhost:3000/usuarios';
+  let Json = `http://localhost:3000/usuarios/${id}`;
   fetch(Json)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      if (data[id] && data[id].nome && data[id].sobre && data[id].imagem) {
-        console.log(data);
-        let img = document.getElementById("logo-perfil");
-        let nome = document.getElementById("perfil-nome");
-        let sobre = document.getElementById("desc");
+      let img = document.getElementById("logo-perfil");
+      let name = document.getElementById("perfil-nome");
+      let descri = document.getElementById("desc");
+      console.log(data);
 
-        sobre.innerText = data[id].sobre;
-        img.src = data[id].imagem;
-        nome.innerText = data[id].nome;
-      } else {
-        window.stop();
-        alert("Você não possui um perfil.");
-        window.location.href = "/src/HTML/TelaEditarPerfil.html";
+      if (img) {
+        img.src = `${data.imagem}`;
+        name.textContent = `${data.nome}`;
+        descri.textContent = `${data.sobre}`;
       }
     });
 }
 
 function Servicos(id) {
-  let servicos = 'http://localhost:3000/servicos';
+  let servicos = "http://localhost:3000/servicos";
   fetch(servicos)
     .then(function (response) {
       return response.json();
@@ -53,85 +48,85 @@ function Servicos(id) {
       console.log(data);
 
       let card = document.getElementById("conteudo");
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("newDiv");
       let strHtml = "";
-
-      if (data.length > 0) {
-        for (let i = 0; i < data.length; i++) {
-          const ser = data[i];
-
-          if (ser.title && ser.descricao) {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].idE == id) {
+          console.log("ola");
+          if (data[i].imagem) {
             strHtml += `
-            <div class="card-vagas">
-              <div class="card-body" id="cardvagas">
-                <div class="container text-center">
-                  <div class="row row-cols-2">
-                    <div class="col-md-4">
-                      <img class="icon-vagas" src="${ser.imagem}" />
-                    </div>
-                    <div class="col-md-8">
-                      <h6>${ser[id].title}</h6>
-                      <p id="descVaga">
-                        ${ser[id].descricao}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+            <div class="container-Serv">
+              <div class="Divimg-Serv">
+                <img src="${data[i].imagem}" class="img-Serv" alt="Minha Figura">
               </div>
-            </div>`;
+              <div class="Divdados-serv">
+                <p class="title-serv">${data[i].title}</p>
+                <p class="descri-serv">${data[i].descricao}</p>
+              </div>
+            </div> `;
+          } else {
+            strHtml += `
+            <div class="container-Serv">
+              <div class="Divimg-Serv">
+                <img src="../imgs/icon-user.png" class="img-Serv" alt="Minha Figura">
+              </div>
+              <div class="Divdados-serv">
+                <p class="title-serv">${data[i].title}</p>
+                <p class="descri-serv">${data[i].descricao}</p>
+              </div>
+            </div>
+            `;
           }
         }
-      } else {
-        strHtml = "<p>O usuario ainda não possui serviços</p>";
       }
-
-      card.innerHTML = strHtml;
-      console.log(card);
+      newDiv.innerHTML = strHtml;
+      card.appendChild(newDiv);
     });
 }
 
+// function Vagas(id) {
+//   let vagas = "http://localhost:3000/vagas";
+//   fetch(vagas)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
 
-function Vagas(id) {
-  let vagas = 'http://localhost:3000/vagas';
-  fetch(vagas)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+//       let card = document.getElementById("conteudo");
+//       let strHtml = "";
 
-      let card = document.getElementById("conteudo");
-      let strHtml = "";
+//       if (data.length > 0) {
+//         for (let i = 0; i < data.length; i++) {
+//           const vagaAtual = data[i];
 
-      if (data.length > 0) {
-        for (let i = 0; i < data.length; i++) {
-          const vagaAtual = data[i];
+//           if (vagaAtual.nomeVaga && vagaAtual.descricao) {
+//             strHtml += `
+//             <div class="card-vagas">
+//               <div class="card-body" id="cardvagas">
+//                 <div class="container text-center">
+//                   <div class="row row-cols-2">
+//                     <div class="col-md-4">
+//                       <img class="icon-vagas" src="${vagaAtual[id].imagem}" />
+//                     </div>
+//                     <div class="col-md-8">
+//                       <h6>${vagaAtual[id].nomeVaga}</h6>
+//                       <p id="descVaga">
+//                       ${vagaAtual[id].descricao}
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>`;
+//           }
+//         }
+//       } else {
+//         strHtml = "<p>Nenhuma vaga disponível.</p>";
+//       }
 
-          if (vagaAtual.nomeVaga && vagaAtual.descricao) {
-            strHtml += `
-            <div class="card-vagas">
-              <div class="card-body" id="cardvagas">
-                <div class="container text-center">
-                  <div class="row row-cols-2">
-                    <div class="col-md-4">
-                      <img class="icon-vagas" src="${vagaAtual[id].imagem}" />
-                    </div>
-                    <div class="col-md-8">
-                      <h6>${vagaAtual[id].nomeVaga}</h6>
-                      <p id="descVaga">
-                      ${vagaAtual[id].descricao}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>`;
-          }
-        }
-      } else {
-        strHtml = "<p>Nenhuma vaga disponível.</p>";
-      }
-
-      card.innerHTML = strHtml;
-      console.log(card);
-    });
-}
+//       card.innerHTML = strHtml;
+//       console.log(card);
+//     });
+// }
