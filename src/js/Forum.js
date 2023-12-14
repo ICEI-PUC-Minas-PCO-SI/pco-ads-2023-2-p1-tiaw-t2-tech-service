f37ae2b701e3b793ebc1447dfdc9b2a2a8b71787
RafaeL Brandao <rbs13bg13@gmail.com>
2023-12-13 22:01:30 -0300

    Criação tela de denúncia

--- src/js/Forum.js
+++ src/js/Forum.js
@@ -1,8 +1,12 @@
 //Recuperando ID do usuario
 let params = new URLSearchParams(location.search);
-console.log(params);
 let id = params.get("id");
-console.log(id);
+let aPerfil = document.getElementById("a-Perfil");
+aPerfil.href = `Perfil.Freelancer.html?id=${id}`;
+let aVagas = document.getElementById("a-Vagas");
+aVagas.href = `VagasDisponiveis.html?id=${id}`;
+let aDenuncia = document.getElementById("a-Denuncia");
+aDenuncia.href = `Denuncia.html?id=${id}`;
 
 //Recuperando ID do usuario
 function returnToMainPage() {
@@ -17,119 +21,111 @@ function closeForm() {
 }
 
 function filterAll() {
-  fetch('http://localhost:3000/posts')
-  .then(response => response.json())
-      .then(posts => {
-              updateUI(posts);
-            });
-  }
+  fetch("http://localhost:3000/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      updateUI(posts);
+    });
+}
 
 function updateUI(posts) {
+  var forumContainer = document.querySelector(".container");
+  forumContainer.innerHTML = "";
 
-  var forumContainer = document.querySelector('.container');
-    forumContainer.innerHTML = '';
+  posts.forEach((post) => {
+    var forumDiv = document.createElement("div");
+    forumDiv.className = "forum";
 
-  posts.forEach(post => {
-        var forumDiv = document.createElement('div');
-        forumDiv.className = 'forum';
+    var titleHeading = document.createElement("h2");
+    titleHeading.textContent = post.title;
 
-                    var titleHeading = document.createElement('h2');
-        titleHeading.textContent = post.title;
+    var contentParagraph = document.createElement("p");
+    contentParagraph.textContent = post.content;
 
-                    var contentParagraph = document.createElement('p');
-        contentParagraph.textContent = post.content;
+    var infoDiv = document.createElement("div");
+    infoDiv.className = "info";
 
-                    var infoDiv = document.createElement('div');
-        infoDiv.className = 'info';
+    var commentsSpan = document.createElement("span");
+    commentsSpan.textContent = "Comentários: " + post.commentsCount;
 
-                    var commentsSpan = document.createElement('span');
-        commentsSpan.textContent = 'Comentários: ' + post.commentsCount; 
+    var likesSpan = document.createElement("span");
+    likesSpan.textContent = "Curtidas: " + post.likesCount;
 
-                    var likesSpan = document.createElement('span');
-        likesSpan.textContent = 'Curtidas: ' + post.likesCount; 
+    var viewsSpan = document.createElement("span");
+    viewsSpan.textContent = "Visualizações: " + post.viewsCount;
 
-                    var viewsSpan = document.createElement('span');
-        viewsSpan.textContent = 'Visualizações: ' + post.viewsCount;
+    infoDiv.appendChild(commentsSpan);
+    infoDiv.appendChild(likesSpan);
+    infoDiv.appendChild(viewsSpan);
 
-                    infoDiv.appendChild(commentsSpan);
-        infoDiv.appendChild(likesSpan);
-        infoDiv.appendChild(viewsSpan);
+    forumDiv.appendChild(titleHeading);
+    forumDiv.appendChild(contentParagraph);
+    forumDiv.appendChild(infoDiv);
 
-                    forumDiv.appendChild(titleHeading);
-        forumDiv.appendChild(contentParagraph);
-        forumDiv.appendChild(infoDiv);
-
-                    forumContainer.appendChild(forumDiv);
-      });
-  }
+    forumContainer.appendChild(forumDiv);
+  });
+}
 
 function filterRecent() {
+  fetch("http://localhost:3000/posts?_sort=date&_order=desc")
+    .then((response) => response.json())
+    .then((recentPosts) => {
+      updateUI(recentPosts);
+    });
+}
 
-  fetch('http://localhost:3000/posts?_sort=date&_order=desc')
-      .then(response => response.json())
-      .then(recentPosts => {
-
-                  updateUI(recentPosts);
-            });
-  }
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-                            filterAll(); 
-                            setupEventListeners(); 
-                          });
+document.addEventListener("DOMContentLoaded", function () {
+  filterAll();
+  setupEventListeners();
+});
 
 function renderPosts(posts) {
+  var forumContainer = document.querySelector(".container");
+  forumContainer.innerHTML = "";
 
-  var forumContainer = document.querySelector('.container');
-    forumContainer.innerHTML = '';
+  posts.forEach((post) => {
+    var forumDiv = document.createElement("div");
+    forumDiv.className = "forum";
 
-  posts.forEach(post => {
-        var forumDiv = document.createElement('div');
-        forumDiv.className = 'forum';
+    var titleHeading = document.createElement("h2");
+    titleHeading.textContent = post.title;
 
-                    var titleHeading = document.createElement('h2');
-        titleHeading.textContent = post.title;
+    var contentParagraph = document.createElement("p");
+    contentParagraph.textContent = post.content;
 
-                    var contentParagraph = document.createElement('p');
-        contentParagraph.textContent = post.content;
+    var infoDiv = document.createElement("div");
+    infoDiv.className = "info";
 
-                    var infoDiv = document.createElement('div');
-        infoDiv.className = 'info';
+    var commentsSpan = document.createElement("span");
+    commentsSpan.textContent = "Comentários: " + post.commentsCount;
 
-                    var commentsSpan = document.createElement('span');
-        commentsSpan.textContent = 'Comentários: ' + post.commentsCount;
+    var likesSpan = document.createElement("span");
+    likesSpan.textContent = "Curtidas: " + post.likesCount;
 
-                    var likesSpan = document.createElement('span');
-        likesSpan.textContent = 'Curtidas: ' + post.likesCount;
+    var viewsSpan = document.createElement("span");
+    viewsSpan.textContent = "Visualizações: " + post.viewsCount;
 
-                    var viewsSpan = document.createElement('span');
-        viewsSpan.textContent = 'Visualizações: ' + post.viewsCount;
+    infoDiv.appendChild(commentsSpan);
+    infoDiv.appendChild(likesSpan);
+    infoDiv.appendChild(viewsSpan);
 
-                    infoDiv.appendChild(commentsSpan);
-        infoDiv.appendChild(likesSpan);
-        infoDiv.appendChild(viewsSpan);
+    forumDiv.appendChild(titleHeading);
+    forumDiv.appendChild(contentParagraph);
+    forumDiv.appendChild(infoDiv);
 
-                    forumDiv.appendChild(titleHeading);
-        forumDiv.appendChild(contentParagraph);
-        forumDiv.appendChild(infoDiv);
-
-                    forumContainer.appendChild(forumDiv);
-      });
-  }
+    forumContainer.appendChild(forumDiv);
+  });
+}
 
 function fetchAndRenderPosts(url) {
-    fetch(url)
-      .then(response => response.json())
-      .then(posts => {
-              renderPosts(posts);
-            })
-      .catch(error => {
-              console.error('Erro ao buscar posts:', error);
-            });
-  }
-
-function setupEventListeners() {
-
+  fetch(url)
+    .then((response) => response.json())
+    .then((posts) => {
+      renderPosts(posts);
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar posts:", error);
+    });
 }
+
+function setupEventListeners() {}
